8b200d74968e38f1823add4a9879028513c07da1
Arkady Emelyanov <arkady.emelyanov@dalee.ru>
2016-09-09 16:10:19 +0300

    python SNI support for SSL

--- scripts/ubuntu14/vagrant.sh
+++ scripts/ubuntu14/vagrant.sh
@@ -7,6 +7,14 @@ export DEBIAN_FRONTEND=noninteractive && \
 	apt-get -qq -y -o Dpkg::Options::="--force-confold" upgrade && \
 	apt-get -qq -y clean
 
+# upgrade python dependencies
+export DEBIAN_FRONTEND=noninteractive && \
+	update-ca-certificates --fresh && \
+	apt-get -qq -y install python-dev libffi-dev libssl-dev python-pip && \
+	pip install --upgrade urllib3 pyopenssl ndg-httpsclient pyasn1 && \
+	apt-get -qq -y remove python-dev libffi-dev libssl-dev && \
+	apt-get -qq -y clean
+
 # install vagrant insecure key, it will be replaced during initial "vagrant up"
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate -O authorized_keys "https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub"
