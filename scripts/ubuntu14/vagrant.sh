691ca01cd47fa5ea2c00f6a0941634e21fb1dcaf
Arkady Emelyanov <arkady.emelyanov@dalee.ru>
2016-09-09 15:23:33 +0300

    update script

--- scripts/ubuntu14/vagrant.sh
+++ scripts/ubuntu14/vagrant.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -eux
 
+# upgrade system
+export DEBIAN_FRONTEND=noninteractive && \
+	sed 's/archive.ubuntu.com/mirror.yandex.ru/' -i /etc/apt/sources.list && \
+	apt-get -qq -y update && \
+	apt-get -qq -y -o Dpkg::Options::="--force-confold" upgrade && \
+	apt-get -qq -y clean
+
 # install vagrant insecure key, it will be replaced during initial "vagrant up"
 mkdir /home/vagrant/.ssh
 wget --no-check-certificate -O authorized_keys "https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub"
