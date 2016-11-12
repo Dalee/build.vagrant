89bd7ebb2136ba056a846fa8955b3c307534c154
Arkady Emeleyanov <arkady.emelyanov@dalee.ru>
2016-11-13 02:38:43 +0300

    centos and debian - removed

--- scripts/centos7/vagrant.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# install vagrant insecure key, it will be replaced during initial "vagrant up"
-mkdir /home/vagrant/.ssh
-curl -o /home/vagrant/.ssh/authorized_keys -kL "https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub"
-chown -R vagrant:vagrant /home/vagrant/.ssh
-chmod 0700 /home/vagrant/.ssh
-chmod 0600 /home/vagrant/.ssh/authorized_keys
